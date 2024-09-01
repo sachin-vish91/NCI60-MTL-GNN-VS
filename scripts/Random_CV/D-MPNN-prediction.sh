dd1b0041afe187cfd1eb7b55a0ce8bde6a5112fb
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-09-01 12:32:59 +0200

    Create D-MPNN-prediction.sh

---
+++ scripts/Random_CV/D-MPNN-prediction.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=train-HCT-116
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=50
+#SBATCH --partition=fast
+#SBATCH --mem=150GB
+
+# Activate conda env
+conda activate chemprop
+
+chemprop_predict --test_path test.csv --checkpoint_path ../model.pt --preds_path prediction.csv
