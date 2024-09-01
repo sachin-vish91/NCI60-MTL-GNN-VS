f414a7a04d4f8a5a58afb51724f7d9307fa744ab
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-09-01 12:50:47 +0200

    Update D-MPNN-train.sh

--- scripts/Random_CV/D-MPNN-train.sh
+++ scripts/Random_CV/D-MPNN-train.sh
@@ -8,4 +8,22 @@
 # Activate conda env
 conda activate chemprop
 
-chemprop_train --data_path train_dataset.csv --config_path  Config.json --smiles_columns SMILE --target_columns NLOGGI50 --dataset_type regression --epochs 100 --save_dir ./output_dir --split_sizes 0.9 0.1 0.0
+
+#CNA
+chemprop_train --data_path ../CNA/CNA_train.csv --features_path ../CNA/CNA_train_profile.csv --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --save_dir ../CNA/check_point --split_sizes 0.9 0.1 0.0 --no_features_scaling --epochs 50 
+
+
+#EXSNP
+#chemprop_train --data_path ../EXSNP/EXSNP_train.csv --features_path ../EXSNP/EXSNP_train_profile.csv --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --save_dir ../EXSNP/check_point --split_sizes 0.9 0.1 0.0 --no_features_scaling --epochs 50 
+
+
+#Methy
+#chemprop_train --data_path ../Methy/Methy_train.csv --features_path ../Methy/Methy_train_profile.csv --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --save_dir ../Methy/check_point --split_sizes 0.9 0.1 0.0 --no_features_scaling --epochs 50 
+
+
+#Protein
+#chemprop_train --data_path ../Protein/Protein_train.csv --features_path ../Protein/Protein_train_profile.csv --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --save_dir ../Protein/check_point --split_sizes 0.9 0.1 0.0 --no_features_scaling --epochs 50 
+
+
+#GeneExp
+#chemprop_train --data_path ../GeneExp/GeneExp_train.csv --features_path ../GeneExp/GeneExp_train_profile.csv --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --save_dir ../GeneExp/check_point --split_sizes 0.9 0.1 0.0 --no_features_scaling --epochs 50 
