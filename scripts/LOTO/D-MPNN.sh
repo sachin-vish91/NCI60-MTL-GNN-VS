787f4f58bb22ab0aefb3cbdd165f1fc1048329c5
Sachin Vishwakarma <sachin.vish91@gmail.com>
2024-09-01 16:58:15 +0200

    Update D-MPNN.sh

--- scripts/LOTO/D-MPNN.sh
+++ scripts/LOTO/D-MPNN.sh
@@ -8,8 +8,10 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=user-email
 
-### The example below is for breast cancer, where the user needs to concatenate all cell lines that belong to this tissue type. Users should repeat this process for each tissue type and molecular profile.
+### Activate conda env
+conda activate chemprop
 
+### The example below is for breast cancer, where the user needs to concatenate all cell lines that belong to this tissue type. Users should repeat this process for each tissue type and molecular profile.
 
 chemprop_train --data_path ../CNA/breast-cancer_train.csv --features_path ../CNA/breast-cancer_train_profile.csv --save_dir ../CNA/breast-cancer --no_features_scaling  --smiles_columns STD_SMILE --target_columns NLOGGI50_N --dataset_type regression --split_sizes 0.9 0.1 0.0 --no_features_scaling --epochs 30
 
